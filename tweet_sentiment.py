4b6bc5a34860a917e61f29988908242782cf7af8
aceakshat <aceakshat@gmail.com>
2014-07-15 10:04:44 -0600

    Update tweet_sentiment.py
    data science assignment 1.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,17 +1,36 @@
-import sys
+import sys, json
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+    
+def parse_dictinory(dict_file):
+    scores = {} # initialize an empty dictionary
+    for line in dict_file:
+      term, score  = line.split("\t")
+      scores[term] = int(score)  # Convert the score to an integer.
+    return scores
+  
+def score_sentiment(tweet, word_score):
+  sentiment_score = 0
+  for word in tweet.split():    
+    if(word_score.has_key(word)):
+      sentiment_score = sentiment_score + word_score[word]
+  return sentiment_score
 
 def main():
-    sent_file = open(sys.argv[1])
+    word_score = parse_dictinory(open(sys.argv[1]))
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    tweet_data = json.load(tweet_file)
+    score_List = list() # empty list for storing result
+
+    for x in tweet_data:
+      if x.has_key("text"):
+   	    score_List.append(score_sentiment(x["text"], word_score))
+      else:
+	    score_List.append(0)
+    
+    for score in score_List:
+ 	  print score
+      
 
 if __name__ == '__main__':
     main()
